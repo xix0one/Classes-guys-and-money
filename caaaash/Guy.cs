558f5e14ff342d2d62b0b0e640e8ebaa64380f3e
xix0one <petrovalex2014@ya.ru>
2024-01-30 20:39:12 +0800

    Добавьте файлы проекта.

---
+++ caaaash/Guy.cs
@@ -0,0 +1,41 @@
+﻿namespace caaaash
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks!");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says {amount} isn't a valid amount!");
+                return 0;
+            }
+            else if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount}");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount I'll take!");
+            }
+            else 
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ caaaash/Program.cs
@@ -0,0 +1,36 @@
+﻿using caaaash;
+
+Guy Joe = new Guy() { Cash = 50, Name = "Joe"};
+Guy Bob = new Guy() { Cash = 100, Name = "Bob"};
+
+while (true)
+{
+    Joe.WriteMyInfo();
+    Bob.WriteMyInfo();
+
+    Console.Write("Enter an amount: ");
+    string howMuch = Console.ReadLine();
+    if (howMuch == "")
+        return;
+    if (int.TryParse(howMuch, out int sum))
+    {
+        Console.Write("Who should give the cash: ");
+        string whichGuy = Console.ReadLine();
+        if (whichGuy == "Joe")
+        {
+            Joe.GiveCash(sum);
+            Bob.ReceiveCash(sum);
+        }
+        else if (whichGuy == "Bob")
+        {
+            Bob.GiveCash(sum);
+            Joe.ReceiveCash(sum);
+        }
+        else
+        {
+            Console.WriteLine("Please enter 'Joe' or 'Bob'");
+        }
+    }
+    else
+        Console.WriteLine("Please enter an amount (or a blank line to exit).");
+}
